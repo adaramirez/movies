3289f7f6b3223abdfa661f0100a62cfc48e6bab4
adaramirez <adalu.gypsy@gmail.com>
2018-08-27 03:14:12 -0500

    failed attempt to create the modal

--- js/app.js
+++ js/app.js
@@ -22,6 +22,9 @@ xhr.addEventListener("readystatechange", function () {
                         <p class="vote">Vote Average: ${movie.vote_average}</p>
                         <p class="count">Vote Count: ${movie.vote_count}</p>
                         <p class="popularity">Popularity:${movie.popularity}</p>
+                        <button id="modalBtn" type="button" class="btn btn-outline-secondary data-toggle="modal" data-target="#basicExampleModal">
+                         More
+                        </button>
                     </div>
                 </div>
             </div>
@@ -34,6 +37,31 @@ xhr.addEventListener("readystatechange", function () {
             ${list.items.map(moviesTemplate).join('')}
             <footer>These ${list.item_count} movies were added recently. Check back soon for updates</footer>
         `
+        var modal = document.getElementById('movie-extra-info');
+        var modalBtn = document.getElementById('modalBtn');
+        var closeBtn = document.getElementsByClassName('close')[0];
+        modalBtn.addEventListener('click',openModal);
+        closeBtn.addEventListener('click',closeModal);
+        window.addEventListener('click', outsideClick);
+
+        //Function to open Modal
+        function openModal(){
+            modal.style.display ='block';
+        }
+
+        //Function to close modal
+        function closeModal(){
+            modal.style.display = 'none';
+        }
+
+        //Function to close modal if outside click
+        function outsideClick(e){
+            if(e.target == modal){
+                modal.style.display = 'none';
+            }
+        }
+
+
     }
    
 });
